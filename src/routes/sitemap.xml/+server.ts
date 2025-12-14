780800dbf4b7ec86d3b89ee841c6fdb3c6ed6f49
Phisict <adrianfathan69@gmail.com>
2025-12-15 02:30:58 +0700

    Automated Feat: update sitemap generation to use dynamic URL and last modified date

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,21 +1,19 @@
-export async function GET() {
-	const baseUrl = 'https://sm.fana.my.id';
+export async function GET({ url }: { url: URL }) {
+	const origin = url.origin;
+	const lastmod = new Date().toISOString();
 
 	const pages = [
-		{ url: '', priority: '1.0', changefreq: 'weekly' },
-		{ url: '/app', priority: '0.8', changefreq: 'monthly' },
+		{ path: '/', priority: '1.0', changefreq: 'weekly' },
+		{ path: '/app', priority: '0.8', changefreq: 'monthly' }
 	];
 
 	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-        http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${pages
 			.map(
 				(page) => `  <url>
-    <loc>${baseUrl}${page.url}</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
+    <loc>${origin}${page.path}</loc>
+    <lastmod>${lastmod}</lastmod>
     <changefreq>${page.changefreq}</changefreq>
     <priority>${page.priority}</priority>
   </url>`
@@ -25,8 +23,8 @@ ${pages
 
 	return new Response(sitemap, {
 		headers: {
-			'Content-Type': 'application/xml',
-			'Cache-Control': 'max-age=0, s-maxage=3600'
+			'Content-Type': 'application/xml; charset=utf-8',
+			'Cache-Control': 'public, max-age=0, s-maxage=86400, stale-while-revalidate=3600, no-transform'
 		}
 	});
 }
